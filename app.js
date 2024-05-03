bfabbbdd2c9cdbb88c31f337b2e06837b2f8cd0b
Nevish-302 <shiven302@gmail.com>
2024-05-03 15:50:54 +0530

    hh

--- app.js
+++ app.js
@@ -696,6 +696,162 @@ const exampleresponseKerela = {
     }
 }
 
+const responsewithloc = {
+    "places": [
+        {
+            "name": "Golden Temple",
+            "location": "Amritsar",
+            "longitude": "74.8755",
+            "latitude": "31.6209",
+            "image_link": "https://images.pexels.com/photos/574313/pexels-photo-574313.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+            "description": "The Harmandir Sahib, informally referred to as the Golden Temple, is a prominent Sikh gurdwara located in the city of Amritsar, Punjab, India.",
+            "attraction": "Historical, Religious",
+            "VisitTime": "2-3 hours",
+            "childrenAllowed": "Yes"
+        },
+        {
+            "name": "Jallianwala Bagh",
+            "location": "Amritsar",
+            "longitude": "74.8723",
+            "latitude": "31.6251",
+            "image_link": "https://images.pexels.com/photos/2528431/pexels-photo-2528431.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+            "description": "Jallianwala Bagh is a public garden in Amritsar, Punjab, India, and houses a memorial of national importance, established in 1951 by the Government of India, to commemorate the massacre of peaceful celebrators on the occasion of the Baisakhi festival on 13 April 1919, who were fired upon by troops of the British Indian Army.",
+            "attraction": "Historical, Memorial",
+            "VisitTime": "1-2 hours",
+            "childrenAllowed": "Yes"
+        },
+        {
+            "name": "Wagah Border",
+            "location": "Amritsar",
+            "longitude": "74.5823",
+            "latitude": "31.6333",
+            "image_link": "https://images.pexels.com/photos/6373487/pexels-photo-6373487.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+            "description": "The Wagah border ceremony, officially called the Beating Retreat ceremony, is a daily military practice that the security forces of India and Pakistan have jointly followed since 1959.",
+            "attraction": "Cultural, Military",
+            "VisitTime": "1-2 hours",
+            "childrenAllowed": "Yes"
+        },
+        {
+            "name": "Anandpur Sahib",
+            "location": "Rupnagar",
+            "longitude": "76.4531",
+            "latitude": "31.2053",
+            "image_link": "https://images.pexels.com/photos/5125093/pexels-photo-5125093.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+            "description": "Anandpur Sahib is a city in Shaheed Bhagat Singh Nagar district in the state of Punjab, India, founded in 1665 by the ninth Sikh Guru, Guru Tegh Bahadur.",
+            "attraction": "Historical, Religious",
+            "VisitTime": "2-3 hours",
+            "childrenAllowed": "Yes"
+        },
+        {
+            "name": "Kila Raipur Sports Festival",
+            "location": "Ludhiana",
+            "longitude": "75.8574",
+            "latitude": "30.9020",
+            "image_link": "https://images.pexels.com/photos/238636/pexels-photo-238636.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+            "description": "The Kila Raipur Sports Festival, often referred to as the 'Rural Olympics', is an annual sporting event held in Kila Raipur, near Ludhiana, Punjab, India.",
+            "attraction": "Cultural, Sports",
+            "VisitTime": "1-2 days",
+            "childrenAllowed": "Yes"
+        },
+        {
+            "name": "Virasat-e-Khalsa",
+            "location": "Anandpur Sahib",
+            "longitude": "76.4531",
+            "latitude": "31.2053",
+            "image_link": "https://images.pexels.com/photos/19818017/pexels-photo-19818017.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+            "description": "Virasat-e-Khalsa is a museum and exhibition complex located in Anandpur Sahib, Punjab, India.",
+            "attraction": "Cultural, Educational",
+            "VisitTime": "2-3 hours",
+            "childrenAllowed": "Yes"
+        },
+        {
+            "name": "Rock Garden of Chandigarh",
+            "location": "Chandigarh",
+            "longitude": "76.7794",
+            "latitude": "30.7333",
+            "image_link": "https://images.pexels.com/photos/19811777/pexels-photo-19811777.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+            "description": "The Rock Garden of Chandigarh is a sculpture garden in Chandigarh, India, also known as Nek Chand's Rock Garden.",
+            "attraction": "Cultural, Art",
+            "VisitTime": "2-3 hours",
+            "childrenAllowed": "Yes"
+        },
+        {
+            "name": "Sukhna Lake",
+            "location": "Chandigarh",
+            "longitude": "76.7827",
+            "latitude": "30.7433",
+            "image_link": "https://images.pexels.com/photos/22487027/pexels-photo-22487027.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+            "description": "Sukhna Lake in Chandigarh, India, is a reservoir at the foothills of the Shivalik range of the Himalayas.",
+            "attraction": "Natural, Recreational",
+            "VisitTime": "1-2 hours",
+            "childrenAllowed": "Yes"
+        }
+    ],
+    "dayWiseItinerary": [
+        {
+            "day": "Day 1",
+            "places": [
+                "Golden Temple",
+                "Jallianwala Bagh"
+            ]
+        },
+        {
+            "day": "Day 2",
+            "places": [
+                "Wagah Border",
+                "Anandpur Sahib"
+            ]
+        },
+        {
+            "day": "Day 3",
+            "places": [
+                "Kila Raipur Sports Festival"
+            ]
+        },
+        {
+            "day": "Day 4",
+            "places": [
+                "Virasat-e-Khalsa"
+            ]
+        },
+        {
+            "day": "Day 5",
+            "places": [
+                "Rock Garden of Chandigarh",
+                "Sukhna Lake"
+            ]
+        }
+    ],
+    "totalTravelTime": "5 days",
+    "hotels": [
+        "Hotel A",
+        "Hotel B",
+        "Hotel C",
+        "Hotel D",
+        "Hotel E"
+    ],
+    "estimatedCost": {
+        "accomodation": "5000 INR",
+        "activities": "2000 INR",
+        "food": "3000 INR",
+        "transport": "4000 INR"
+    },
+    "experiences": [
+        "Cultural",
+        "Historical",
+        "Religious",
+        "Sports",
+        "Art"
+    ],
+    "transport": [
+        "Flight",
+        "Train",
+        "Cab",
+        "Auto-rickshaw",
+        "Walking"
+    ]
+}
+
 const schema = {
 
     properties: {
